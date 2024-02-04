6051f7717c4be581d40ed3f1a651c122de4ff69d
V11nx <thegreatAPEX1@gmail.com>
2024-02-04 15:36:27 -0600

    noteintake work 2

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -15,7 +15,6 @@ import frc.robot.subsystems.NoteIntake;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
--- src/main/java/frc/robot/subsystems/NoteIntake.java
+++ src/main/java/frc/robot/subsystems/NoteIntake.java
@@ -7,4 +7,15 @@ import frc.robot.Constants;
 public class NoteIntake extends SubsystemBase {
     public final WPI_TalonSRX IntakeMotor = new WPI_TalonSRX(Constants.nIntakeConstants.kIntakeMotor);
 
+
+        //float (decimal value) value is for the motor controller where 1= 100%  foward, -1 backwards, 0 is off
+public void IntakeNote(float speed){
+    IntakeMotor.set(speed);
+}
+
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
 }
